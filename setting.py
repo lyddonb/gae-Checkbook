a4e7965d7502faf5e29c49a5fd2b9108b921538b
Braxton <blyddon22@gmail.com>
2013-01-17 15:48:19 -0600

    adding the rest of my files

---
+++ setting.py
@@ -0,0 +1,18 @@
+
+import logging
+import os
+import sys
+
+# Add lib to path.
+libs_dir = os.path.join(os.path.dirname(__file__), 'lib')
+if libs_dir not in sys.path:
+    logging.debug('Adding lib to path.')
+    sys.path.insert(0, libs_dir)
+
+import webapp2
+
+url_map = [
+    ('.*/mybook', 'mybook.MainPage'),
+]
+
+app = webapp2.WSGIApplication(url_map)
