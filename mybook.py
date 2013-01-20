9bdbc10b22dd93449ed6872573a394a961938d76
Braxton <blyddon22@gmail.com>
2013-01-20 16:20:43 -0600

    Removed mybook.py

--- mybook.py
+++
@@ -1,8 +0,0 @@
-import webapp2
-from google.appengine.ext.webapp import template
-
-class MainPage(webapp2.RequestHandler):
-	def get(self):
-		self.response.out.write(template.render('mybook.html', {}))
-
-app = webapp2.WSGIApplication([('/', MainPage)], debug = True)
